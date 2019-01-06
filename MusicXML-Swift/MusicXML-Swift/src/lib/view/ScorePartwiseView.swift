264b2ca307cd419123a5d804bdcfe867247cddc9
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2019-01-07 01:11:17 +0200

    Basic staff closing added.

--- MusicXML-Swift/MusicXML-Swift/src/lib/view/ScorePartwiseView.swift
+++ MusicXML-Swift/MusicXML-Swift/src/lib/view/ScorePartwiseView.swift
@@ -13,23 +13,33 @@ class ScorePartwiseView: UIView {
     fileprivate var scorePartwise: ScorePartwise!
 
     fileprivate let color = UIColor.black
+    let kStaffSpace: CGFloat = 10.0
+    let kStaffLineWidth: CGFloat = 1.0
+    let kStaffTopOffset: CGFloat =  20.0
+    let kStaffSideOffset: CGFloat = 10.0
+    let kLinesNumber: Int = 5
+
+    // MARK: - Public API
 
     func update(_ aScorePartwise: ScorePartwise) {
         scorePartwise = aScorePartwise
     }
 
+    // MARK: - From base clsses
+
     override func draw(_ rect: CGRect) {
         guard scorePartwise != nil else { return }
 
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        drawText(context: context,
+        drawText(context,
                  text: scorePartwise.partList.scorePart.partName.partNameText)
-        drawStaff(context: context)
+        drawStaff(context)
+        closeLastStaff(context)
     }
 
     // MARK: - Private API
 
-    fileprivate func drawText(context: CGContext, text: String) {
+    fileprivate func drawText(_ context: CGContext, text: String) {
 
         translate(context: context)
 
@@ -47,23 +57,35 @@ class ScorePartwiseView: UIView {
         CTFrameDraw(frame, context)
     }
 
-    fileprivate func drawStaff(context: CGContext) {
+    fileprivate func drawStaff(_ context: CGContext) {
 
         translate(context: context)
 
-        let kStaffSpace: CGFloat = 10.0
-        let kStaffLineWidth: CGFloat = 1.0
-        let kTopOffset: CGFloat =  20.0
-        let kSideOffset: CGFloat = 10.0
         context.setStrokeColor(color.cgColor)
         context.setLineWidth(kStaffLineWidth)
 
-        for i in 1...5 {
-            let y = kTopOffset + CGFloat(i) * kStaffSpace
-            context.move(to: CGPoint(x: kSideOffset, y: y))
-            context.addLine(to: CGPoint(x: frame.size.width - kSideOffset, y: y))
+        for i in 0...kLinesNumber-1 {
+            let y = kStaffTopOffset + CGFloat(i) * kStaffSpace
+            context.move(to: CGPoint(x: kStaffSideOffset, y: y))
+            context.addLine(to: CGPoint(x: frame.size.width - kStaffSideOffset, y: y))
             context.drawPath(using: .fillStroke)
         }
+    }
+
+    fileprivate func closeLastStaff(_ context: CGContext) {
+
+        let kRectWidth: CGFloat = 3.0
+
+        let x: CGFloat = frame.size.width - kStaffSideOffset - kRectWidth
+        let y: CGFloat = CGFloat(kLinesNumber - 1) * kStaffSpace
+        context.addRect(
+            CGRect(x: x, y: kStaffTopOffset, width: kRectWidth, height: y))
+
+        let xCloseLine: CGFloat = x - 3.0
+        context.move(to: CGPoint(x: xCloseLine, y: kStaffTopOffset))
+        context.addLine(to: CGPoint(x: xCloseLine, y: y + kStaffTopOffset))
+
+        context.drawPath(using: .fillStroke)
     }
 
     fileprivate func translate(context: CGContext) {
